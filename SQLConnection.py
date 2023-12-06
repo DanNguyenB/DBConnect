440ad3f8896555cbf9cc579e523eeec185e6e5e4
Dan Nguyen <dan.nguyen.b@gmail.com>
2023-12-06 07:15:07 -0500

    Completed adding row functionality and started deletion

--- SQLConnection.py
+++ SQLConnection.py
@@ -6,6 +6,7 @@ Created on Mon Dec  4 14:55:19 2023
 """
 
 import pyodbc
+from datetime import datetime
 
 
 # Creating a connection to the "PracticeDatabase" Database
@@ -14,19 +15,47 @@ connection = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};"
                             "Database=PracticeDatabase;"
                             "Trusted_Connection=yes;")
 
-# Executing a query to select the whole table
-#cursor = connection.cursor()
-#cursor.execute("SELECT * FROM [PracticeDatabase].[dbo].[SalesOrders$]")
-#row = cursor.fetchone() 
-#while row:
-    #print (row) 
-    #row = cursor.fetchone()
+isDone = False
+#while isDone == False:
+    
+#choice = input("Please enter an option for your list of sales: \n1. Read table \n2. Add a new entry \n3. Update an entry\
+               #\n4. Delete an entry \n5. Exit \n")
+
+#if choice == '1':
+    # Executing a query to select the whole table
+    #cursor = connection.cursor()
+    #cursor.execute("SELECT * FROM [PracticeDatabase].[dbo].[SalesOrders$]")
+    #row = cursor.fetchone() 
+    #while row:
+        #print (row) 
+        #row = cursor.fetchone()
+
+#elif choice == '2':
+
+#else
     
 # Inserting a new row into the table
+region = input("Please enter the region: ")
+rep = input("Please enter the name of the sales representative: ")
+item = input("Please enter the name of the item: ")
+units = float(input("Please enter the number of items that have been bought: "))
+cost = float(input("Please enter the cost of the item: "))
+total = units * cost
+date = datetime.now()
+
 cursor = connection.cursor()
-cursor.execute("INSERT INTO [PracticeDatabase].[dbo].[SalesOrders$] DEFAULT VALUES")
+cursor.execute("INSERT INTO [PracticeDatabase].[dbo].[SalesOrders$] ([OrderDate], [Region], [Rep], [Item], [Units], [Unit Cost], [Total])\
+               VALUES (?, ?, ?, ?, ?, ?, ?)", (date, region, rep, item, units, cost, total))
 connection.commit()
 
-#(2023, 12, 5, 0, 0)
+# Update row
+#cursor = connection.cursor()
+
+
+# Delete Row
+#cursor = connection.cursor()
+#cursor.execute("DELETE FROM [PracticeDatabase].[dbo].[SalesOrders$] WHERE Total = 8.5")
+#connection.commit()
+
 
 
